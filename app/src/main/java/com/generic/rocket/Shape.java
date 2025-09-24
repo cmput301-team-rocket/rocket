67ee6860f6dae04eeedd2fd01ffe8f10573fe0a4
GenericConfluent <m13power@gmail.com>
2025-09-23 18:10:57 -0600

    feat: make shape purple

--- app/src/main/java/com/generic/rocket/Shape.java
+++ app/src/main/java/com/generic/rocket/Shape.java
@@ -1,7 +1,9 @@
 package com.generic.rocket;
 
+import android.graphics.Color;
+
 public abstract class Shape {
     int x;
     int y;
-    String color = "magenta";
+    String color = "Purple";
 }
